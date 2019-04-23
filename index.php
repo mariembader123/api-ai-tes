669ad4cfff10909284f460ac9ffa4c0d17031acf
bader mariem <bader.mariem1@gmail.com>
2019-04-23 10:52:49 +0200

    tt

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	$text = $json->result->parameters->text;
+	$text = $json->queryResult->queryResult;
 
 	switch ($text) {
 		case 'hi':
@@ -23,7 +23,7 @@ if($method == 'POST'){
 			break;
 		
 		default:
-			$speech = ($json->result->parameters->text)."";
+			$speech = ($json->queryResult->queryResult)."";
 			break;
 	}
 
